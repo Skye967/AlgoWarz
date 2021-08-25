b0ee34f6e2a976bb31f07cf8ecf171c396b52e7d
Skye <81345074+Skye967@users.noreply.github.com>
2021-08-25 08:52:08 -1000

    Add files via upload

---
+++ PrimesInNumbers.js
@@ -0,0 +1,12 @@
+
+
+function primeFactors ( n ) {
+    let arr = [];
+    let prime = 2;
+    let result = n;
+    while ( result != 0 ) {
+        let newNum = result / prime;
+        if(newNum % 1 )
+    }
+}
+primeFactors( 100 );
