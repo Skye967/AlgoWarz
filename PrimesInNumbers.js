bdc72d8b391931107faa579a01a185f05ed96ea6
Skye <81345074+Skye967@users.noreply.github.com>
2021-09-09 15:17:03 -1000

    Add files via upload

--- PrimesInNumbers.js
+++ PrimesInNumbers.js
@@ -1,12 +1,54 @@
 
 
-function primeFactors ( n ) {
+//Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+//Prime factorization of 10 is 2 x 5
+//Prime factorization of 20 is 2 x 2 x 5 or 22 x 51
+
+function primeFactors( n ) {
+    let value = n;
     let arr = [];
-    let prime = 2;
-    let result = n;
-    while ( result != 0 ) {
-        let newNum = result / prime;
-        if(newNum % 1 )
+    for ( let i = 2; i < n + 1; i++ ){
+        let result = value / i;
+        if ( result % 1 == 0 ) {
+            value = result;
+            arr.push( i );
+            if ( value == 1 ) {
+                console.log( arr );
+                break;
+            }
+            i--;
+        }
     }
+    let primeSting = "";
+    let numCount = 1;
+    for ( let j = 0; j < arr.length; j++ ){
+        let num = arr[j];
+        if ( num == arr[ j + 1 ] ) {
+            numCount++;
+        }
+        if ( num != arr[ j + 1 ] ) {
+            if ( numCount == 1 ) {
+                primeSting = primeSting + `(${ num })`;
+            } else {
+                primeSting = primeSting + `(${ num }**${ numCount })`;
+            }
+            numCount = 1;
+        }
+    }
+    console.log( primeSting );
+    return primeSting;
 }
-primeFactors( 100 );
+primeFactors( 86240 );
+
+
+function primeFactors( n ) {
+    for ( var i = 2, res = "", f; i <= n; i++ ) {
+        f = 0;
+        while ( n % i == 0 ) {
+            f++;
+            n /= i
+        }
+        res += f ? "(" + ( f > 1 ? i + "**" + f : i ) + ")" : ""
+    }
+    return res || "(" + n + ")"
+}
